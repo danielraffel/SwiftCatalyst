b7da1d6ac8d50ff07047d6560678e8303d645bc8
daniel raffel <daniel.raffel@gmail.com>
2025-04-25 12:43:22 -0700

    WIP: Local edits before pulling remote changes

--- Sources/App.swift
+++ Sources/App.swift
@@ -4,7 +4,16 @@ import Inject
 @main
 struct SwiftUIViperApp: App {
   @ObserveInjection var inject
-  
+
+  init() {
+    #if DEBUG
+    // Try to load injection bundle from app resources
+    if let path = Bundle.main.path(forResource: "iOSInjection", ofType: "bundle") {
+      Bundle(path: path)?.load()
+    }
+    #endif
+  }
+
   var body: some Scene {
     WindowGroup {
       HomeRouter.createHomeView()
